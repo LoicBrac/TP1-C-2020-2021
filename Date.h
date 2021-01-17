ea56e3b1c37bba8665622703e7161ea1adcfb3fc
hugoCAZES <75845312+hugoCAZES@users.noreply.github.com>
2021-01-17 22:36:15 +0100

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,34 +1,31 @@
 #ifndef DATE_H
 #define DATE_H
 #include <string>
+#include <iostream>
 
-namespace date {
-	class Date {
-	private:
-		int _year;
-		int _month;
-		int _day;
-	public:
-		Date(int year, int month, int day);
-		Date() = default;
-		~Date();
-		int year() const;
-		int month() const;
-		int day() const;
-		bool checkDate(int year, int month, int day);
-		void setYear(int year);
-		void setMonth(int month);
-		void setDay(int day);
-		void nextDay();
-		std::string toString() const;
-	};
+   class Date {
+   private:
+      int _annee;
+      int _mois;
+      int _jour;
+   public:
+      Date (int annee, int mois, int jour);
+      int getAnnee() const;
+      int getMois() const;
+      int getJour() const;
+      bool checkDate(int annee, int mois, int jour);
+      void setAnnee(int annee);
+      void setMois(int mois);
+      void setJour(int jour);
+      void afficherDate();
+      std::string toString() const;
+   };
 
-	bool operator == (const Date& d1, const Date& d2); // comparison operators
-	bool operator != (const Date& d1, const Date& d2);
-	bool operator < (const Date& d1, const Date& d2);
-	bool operator > (const Date& d1, const Date& d2);
-	bool operator <= (const Date& d1, const Date& d2);
-	bool operator >= (const Date& d1, const Date& d2);
+   bool operator == (const Date& d1, const Date& d2);
+   bool operator < (const Date& d1, const Date& d2);
+   bool operator > (const Date& d1, const Date& d2);
+   bool operator <= (const Date& d1, const Date& d2);
+   bool operator >= (const Date& d1, const Date& d2);
 
-}
+
 #endif // DATE_H
